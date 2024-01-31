dd0de0a31a9ded477b2459d90b2714259d3b0071
rouman1201082 <chernovrn@gmail.com>
2024-01-31 18:30:45 +0400

    31.01.24_1

--- einstein.js
+++ einstein.js
@@ -1,4 +1,5 @@
 const myWeight = 60;
 const speedLight = 3e8;
 const energy = (myWeight * (speedLight ** 2));
+
 console.log('Энергия моего тела ', energy);
--- tasks.js
+++ tasks.js
@@ -1,23 +1,30 @@
 'use strict';
+{
+  const rain = Math.random();
 
-const rain = Math.random();
-const mathematicsPoint = +prompt('Введите кол-во баллов по математике:');
-const russianLanguagePoint = +prompt('Введите кол-во баллов по русскому языку:');
-const InformaticsPoint = +prompt('Введите кол-во баллов по информатике:');
-const minSumm = +prompt('Выберите сумму для снятия:');
-
-if (Math.round(rain) === 0) {
-  console.log('Дождя нет!')
-} else { console.log('Пошёл дождь. Возьмите зонт!') };
-
-if (mathematicsPoint + russianLanguagePoint + InformaticsPoint >= 265) {
-  console.log('Поздравляю, вы поступили на бюджет!')
-} else {
-  console.log('К сожалению, вы не поступили на бюджет!')
+  if (Math.round(rain) === 0) {
+    console.log('Дождя нет!')
+  } else { console.log('Пошёл дождь. Возьмите зонт!') };
 };
 
-if (minSumm >= 100) {
-  console.log('Распечатать чек?')
-} else {
-  console.log('Минимальная сумма для снятия 100р')
+{
+  const mathematicsPoint = +prompt('Введите кол-во баллов по математике:');
+  const russianLanguagePoint = +prompt('Введите кол-во баллов по русскому языку:');
+  const informaticsPoint = +prompt('Введите кол-во баллов по информатике:');
+
+  if (mathematicsPoint + russianLanguagePoint + informaticsPoint >= 265) {
+    console.log('Поздравляю, вы поступили на бюджет!')
+  } else {
+    console.log('К сожалению, вы не поступили на бюджет!')
+  };
+};
+
+{
+  const summ = +prompt('Выберите сумму для снятия:');
+
+  if (summ % 100) {
+    console.log('Сумма для снятия должна быть кратна 100р')
+  } else {
+    console.log('Распечатать чек?')
+  };
 };
